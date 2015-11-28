2a3eca7812df8aa6b9ee4a9796f2e2a495602ba9
Raz Friman <superraz375@yahoo.com>
2015-11-28 12:42:56 -0600

    Added a config.js file to store configuration variables such as the MongoDB path

---
+++ config.js
@@ -0,0 +1,6 @@
+// TODO - Move these variables to PROCESS.ENV so they are not stored in Source Control
+
+module.exports = {
+    "secret": "secretpassword123KEEPitPRIVATE",
+    "database": "mongodb://root:admin@ds049624.mongolab.com:49624/bears"
+};
