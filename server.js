46154d1cea8a78a4d26c094d1e59090189a4aa7b
Raz Friman <superraz375@yahoo.com>
2015-11-28 12:56:48 -0600

    Refactor server.js to be more modular

--- server.js
+++ server.js
@@ -1,26 +1,42 @@
-// BASE SETUP
-// =============================================================================
+var express     = require('express');
+var https       = require('https');
+var bodyParser  = require('body-parser');
+var mongoose    = require('mongoose');
+var fs          = require('fs');
+var config      = require('./config');
+var port = process.env.PORT || 8081; // set our port
 
-// call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
-var bodyParser = require('body-parser');
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
+// Setup the app so other modules can also access it through exports
+var app = module.exports = express();
+
+// Set the secret variable for using JWT
+app.set('jwtSecret', config.secret);
+
+// connect to database
+mongoose.connect(config.database);
+
+// use body parser so we can get info from POST and/or URL parameters
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8081;        // set our port
+// enable logging
+// TODO
+//app.use(require('./logging'));
 
-var mongoose   = require('mongoose');
-mongoose.connect('mongodb://root:admin@ds049624.mongolab.com:49624/bears'); // connect to our database
-var Bear       = require('./app/models/bear');
+// connect the login routes
+var unAuthRoutes = require('./loginRoutes');
+app.use('/api', unAuthRoutes);
+
+// connect the authorized routers
+var authRoutes = require('./routes');
+//app.use('/api', authRoutes);
+
 
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
-
+var Bear       = require('./app/models/bear');
 
 // middleware to use for all requests
 router.use(function(req, res, next) {
@@ -116,7 +132,6 @@ router.route('/bears/:bear_id')
 // all of our routes will be prefixed with /api
 app.use('/api', router);
 
-// START THE SERVER
-// =============================================================================
+// Start the server
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log('MyRealEstate API Server listening on port: ' + port);
