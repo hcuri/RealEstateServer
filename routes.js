8b0638259730255625a2bd4bc33e878d9a6629c2
Raz Friman <superraz375@yahoo.com>
2015-11-28 21:32:43 -0600

    GET/PUT/DELETE a selected property

--- routes.js
+++ routes.js
@@ -13,13 +13,6 @@ router.get('/', function (req, res) {
     res.json({message: 'YOU ARE AUTHENTICATED!'});
 });
 
-// ONLY FOR DEBUGGING - Show All users
-router.get('/users', function (req, res) {
-    User.find({}, function (err, users) {
-        res.json(users);
-    });
-});
-
 // Current user
 router.get('/users/:userid', function (req, res, next) {
     if (!req.auth) {
@@ -59,6 +52,7 @@ router.route('/users/:userid/properties')
     // Add a new property
     .post(function (req, res, next) {
 
+        // TODO - set these values from the BODY values
         var property = new Property();
         property.title = "";
         property.type = "Apartment";
@@ -110,19 +104,106 @@ router.route('/users/:userid/properties/:propertyid')
             if (err) {
                 return next(err);
             }
-            res.status(200).json(user.properties);
+
+            // Find the specified property
+            var property = user.properties.id(req.params['propertyid']);
+
+            if(property) {
+                res.status(200).json();
+            } else {
+                res.status(404).send();
+            }
         });
     })
 
     // Update the current property
     .put(function (req, res) {
+        if (!req.auth) {
+            // Check the authentication
+            return res.status(401).send();
+        } else if (req.auth._id !== req.params['userid']) {
+            // Check the the userid provided is the same as the current token for the user
+            return res.status(401).send();
+        }
 
+        // Find the current user
+        User.findOne({_id: {$in: [req.auth._id]}}, function (err, user) {
+            if (err) {
+                return next(err);
+            }
+
+            // Find the property by id
+            var property = user.properties.id(req.params['propertyid']);
+
+            // Update the property variables
+            if(property) {
+
+                // TODO - set these values from the BODY values
+                property.title = "SpaceX HQ";
+                property.type = "Facility";
+                property.address = "1 Rocket Rd"
+                property.city = "Hawthorne";
+                property.value = "9999999";
+                property.totalRentPaid = 0;
+                property.monthsPaid = 0;
+                property.rentPayment = 85000.00;
+                property.returnOnInvestment = 0.00;
+                property.tenant = null;
+                property.image = null;
+                property.dateLastPaid = null;
+
+            } else {
+                return res.status(404).send();
+            }
+
+            // Save the changes to the user document
+            user.save(function (err, savedUser) {
+                if (err) {
+                    return (next(err));
+                }
+                res.status(200).send();
+            });
+        });
 
     })
 
     // Delete the current property
     .delete(function (req, res) {
 
+
+        if (!req.auth) {
+            // Check the authentication
+            return res.status(401).send();
+        } else if (req.auth._id !== req.params['userid']) {
+            // Check the the userid provided is the same as the current token for the user
+            return res.status(401).send();
+        }
+
+        // Find the current user
+        User.findOne({_id: {$in: [req.auth._id]}}, function (err, user) {
+            if (err) {
+                return next(err);
+            }
+
+            // Find the property by id
+            var property = user.properties.id(req.params['propertyid']);
+
+
+            // Remove the property
+            if(property) {
+                property.remove();
+            } else {
+                return res.status(404).send();
+            }
+
+            // Save the changes to the user document
+            user.save(function (err, savedUser) {
+                if (err) {
+                    return (next(err));
+                }
+                res.status(200).send();
+            });
+        });
 
     });
 
